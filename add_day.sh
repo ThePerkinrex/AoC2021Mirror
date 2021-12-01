df6036528f529dae99ca6797e7afcd47aefecb2d
ThePerkinrex <theperkinrex@gmail.com>
2021-12-01 19:18:08 +0100

    Fix script

--- add_day.sh
+++ add_day.sh
@@ -7,25 +7,25 @@ echo "d$1p1:
 	@echo ::: Rust
 	cargo run -p d$1p1 -q || echo Rust not installed
 	@echo ::: Python
-	cd $DIR/d1p1/src && python main.py && cd ../../.. || echo Python not installed
+	cd $DIR/d$1p1/src && python main.py && cd ../../.. || echo Python not installed
 	@echo ::: JavaScipt
-	cd $DIR/d1p1/src && node main.js && cd ../../.. || echo NodeJS not installed
+	cd $DIR/d$1p1/src && node main.js && cd ../../.. || echo NodeJS not installed
 	@echo ::: Java
-	(cd $DIR/d1p1/src && javac Main.java && java Main && rm Main.class && cd ../../..) || echo Java not installed
+	(cd $DIR/d$1p1/src && javac Main.java && java Main && rm Main.class && cd ../../..) || echo Java not installed
 	@echo ::: C#
-	(cd $DIR/d1p1/src && dotnet run && cd ../../..) || echo dotnet not installed
+	(cd $DIR/d$1p1/src && dotnet run && cd ../../..) || echo dotnet not installed
 
 d$1p2:
 	@echo ::: Rust
 	cargo run -p d$1p2 -q || echo Rust not installed
 	@echo ::: Python
-	cd $DIR/d1p2/src && python main.py && cd ../../.. || echo Python not installed
+	cd $DIR/d$1p2/src && python main.py && cd ../../.. || echo Python not installed
 	@echo ::: JavaScipt
-	cd $DIR/d1p2/src && node main.js && cd ../../.. || echo NodeJS not installed
+	cd $DIR/d$1p2/src && node main.js && cd ../../.. || echo NodeJS not installed
 	@echo ::: Java
-	(cd $DIR/d1p2/src && javac Main.java && java Main && rm Main.class && cd ../../..) || echo Java not installed
+	(cd $DIR/d$1p2/src && javac Main.java && java Main && rm Main.class && cd ../../..) || echo Java not installed
 	@echo ::: C#
-	(cd $DIR/d1p2/src && dotnet run && cd ../../..) || echo dotnet not installed
+	(cd $DIR/d$1p2/src && dotnet run && cd ../../..) || echo dotnet not installed
 
 " >> Makefile
 
